5d639de640328f5fd28246e2ce78db9fb75d065a
Bill Horsman <bill@logicalcobwebs.com>
2020-11-12 15:33:33 +0000

    Start again

--- plugins/global.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-import BackHome from '~/components/BackHome.vue'
-Vue.component('back-home', BackHome)
-
-import Avatar from '~/components/Avatar.vue'
-Vue.component('avatar', Avatar)
--- plugins/localStorage.js
+++
@@ -1,8 +0,0 @@
-import createPersistedState from 'vuex-persistedstate'
-
-export default ({store}) => {
-  createPersistedState({
-      key: 'hourjar',
-      paths: []
-  })(store)
-}
