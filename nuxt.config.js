b7b63f154368af59906b5535d4b21cd85ac733cc
Bill Horsman <bill@logicalcobwebs.com>
2020-11-14 13:57:17 +0000

    Add HTML lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,33 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'server',
+  target: "server",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Logical Cobwebs',
+    title: "Logical Cobwebs",
+    lang: "en",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
     link: [
-      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon.png' },
+      { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon.png" },
       {
-        rel: 'stylesheet',
-        href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.0.13/css/all.css",
         integrity:
-          'sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp',
-        crossorigin: 'anonymous',
+          "sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp",
+        crossorigin: "anonymous",
       },
     ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@/assets/stylesheets/typography.scss'],
+  css: ["@/assets/stylesheets/typography.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/global.js'],
+  plugins: ["~/plugins/global.js"],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -34,7 +35,7 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -42,4 +43,4 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
-}
+};
